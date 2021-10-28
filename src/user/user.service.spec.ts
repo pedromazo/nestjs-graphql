c335b38933bbd05c9eaaeab29c92e42cb1d24e5c
Pedro Mazo <pedrolmazo@gmail.com>
2021-10-28 16:42:58 -0300

    First unitary test using Jestgit add .

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,12 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import TestUtil from './../common/test/TestUtil';
+import { User } from './user.entity';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
   let service: UserService;
 
+  const mockRepository = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn()
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [UserService,
+      {
+        provide: getRepositoryToken(User),
+        useValue: mockRepository,
+      },
+    ],
     }).compile();
 
     service = module.get<UserService>(UserService);
@@ -15,4 +32,16 @@ describe('UserService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('findAllUsers', () => {
+    it('should list all users', async () => {
+      const user = TestUtil.giveMeAValidUser();
+      mockRepository.find.mockReturnValue([user, user]);
+      const users = await service.findAllUsers();
+      expect(users).toHaveLength(2);
+      expect(mockRepository.find).toHaveBeenCalledTimes(1);
+
+    })
+  })
+
 });
